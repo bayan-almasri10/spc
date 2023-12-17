c919215e8f865e3229cb74a1b8310ca33fb64249
bayan-almasri10 <114304152+bayan-almasri10@users.noreply.github.com>
2023-12-17 20:29:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
-// function showHideElements() {
-//     let section = document.querySelector(".section");
+// document.addEventListener('DOMContentLoaded', function () {
+//     const toggleCheckbox = document.getElementById('toggleCheckbox');
+//     const toggleButton = document.getElementById('toggleButton');
+//     const elementToToggle = document.getElementById('elementToToggle1');
+
+//     toggleButton.addEventListener('click', function () {
+//     toggleCheckbox.checked = !toggleCheckbox.checked;
+
+//     elementToToggle.classList.toggle('box', !toggleCheckbox.checked);
+//     });
+// });
+
+
+
+
+function toggleElement(elementId) {
+    var element = document.getElementById(elementId);
+
+    if (element) {
+        if (element.style.display === 'none' || element.style.display === '') {
+            element.style.display = 'grid';
+        } else {
+            element.style.display = 'none';
+        }
+    } else {
+        console.error('Element with ID ' + elementId + ' not found.');
+    }
+}
+
+// function toggleElement(elementId) {
+//     var element = document.getElementById("section3");
+
+//     if (element) {
+//         if (element.style.display === 'none' || element.style.display === '') {
+//             element.style.display = 'block';
+//         } else {
+//             element.style.display = 'none';
+//         }
+//     } else {
+//         console.error('Element with ID ' + elementId + ' not found.');
+//     }
+// }
+
+// function showHideElements(sectionSelector) {
+//     let section = document.querySelector(sectionSelector);
 
 //     if (section) {
 //         if (section.style.display === "none" || section.style.display === "") {
-//             section.style.display = "flex";
+//             section.style.display = "block";
 //         } else {
 //             section.style.display = "none";
 //         }
 //     } else {
-//         console.error("Element with class 'section' not found.");
+//         console.error(`Element with selector '${sectionSelector}' not found.`);
 //     }
-// }
-
-function showHideElements(sectionSelector) {
-    let section = document.querySelector(sectionSelector);
-
-    if (section) {
-        if (section.style.display === "none" || section.style.display === "") {
-            section.style.display = "block";
-        } else {
-            section.style.display = "none";
-        }
-    } else {
-        console.error(`Element with selector '${sectionSelector}' not found.`);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let section = document.querySelector(".section");
-// let isShow = true;
-
-// function showHideElements() {
-//     if (isShow) {
-//         section.style.display = "none";
-//         isShow = false;
-//     } else {
-//         section.style.display = "block";
-//         isShow = true;
-//     }
-// }
-
-
-
-// function showHideElements(){
-//     section.classList.toggle("hide");
 // }
